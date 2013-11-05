51537269f8aa160f0c160ab2d4fca91a969f045b
Lionel Ringenbach <ucodia@live.com>
2013-11-05 01:21:07 +0100

    Converted project to portable class library

--- unirest-net/unirest-net/Properties/AssemblyInfo.cs
+++ unirest-net/unirest-net/Properties/AssemblyInfo.cs
@@ -1,6 +1,4 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -13,14 +11,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Microsoft 2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("dcbda267-6634-4eef-8644-a334b398ddef")]
 
 // Version information for an assembly consists of the following four values:
 //
