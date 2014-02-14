ca3631a0c3d1f74df60572f2ba7e4ba596959f96
Lionel Ringenbach <ucodia@live.com>
2014-02-15 00:18:30 +0100

    Splitted projects to target different platforms

---
+++ unirest-net/unirest-net (NET40)/src/http/HttpClientHelper.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+using unirest_net.request;
+
+namespace unirest_net.http
+{
+    public class HttpClientHelper
+    {
+        private const string USER_AGENT = "unirest-net/1.0";
+
+        public static HttpResponse<T> Request<T>(HttpRequest request)
+        {
+            var responseTask = RequestHelper(request);
+            Task.WaitAll(responseTask);
+            var response = responseTask.Result;
+
+            return new HttpResponse<T>(response);
+        }
+
+        public static Task<HttpResponse<T>> RequestAsync<T>(HttpRequest request)
+        {
+            var responseTask = RequestHelper(request);
+            return Task<HttpResponse<T>>.Factory.StartNew(() =>
+            {
+                Task.WaitAll(responseTask);
+                return new HttpResponse<T>(responseTask.Result);
+            });
+        }
+
+        private static Task<HttpResponseMessage> RequestHelper(HttpRequest request)
+        {
+            if (!request.Headers.ContainsKey("user-agent"))
+            {
+                request.Headers.Add("user-agent", USER_AGENT);
+            }
+
+            var client = new HttpClient();
+            var msg = new HttpRequestMessage(request.HttpMethod, request.URL);
+
+            foreach (var header in request.Headers)
+            {
+                msg.Headers.Add(header.Key, header.Value);
+            }
+
+            if (request.Body.Any())
+            {
+                msg.Content = request.Body;
+            }
+
+            return client.SendAsync(msg);
+        }
+    }
+}
---
+++ unirest-net/unirest-net (NET40)/src/http/HttpResponse.cs
@@ -0,0 +1,52 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace unirest_net.http
+{
+    public class HttpResponse<T>
+    {
+        public int Code { get; private set; }
+        public Dictionary<String, String> Headers { get; private set; }
+        public T Body { get; set; }
+        public Stream Raw { get; private set; }
+
+        public HttpResponse(System.Net.Http.HttpResponseMessage response)
+        {
+            Headers = new Dictionary<string, string>();
+            Code = (int)response.StatusCode;
+
+            if (response.Content != null)
+            {
+                var streamTask = response.Content.ReadAsStreamAsync();
+                Task.WaitAll(streamTask);
+                Raw = streamTask.Result;
+
+                if (typeof(T) == typeof(String))
+                {
+                    var stringTask = response.Content.ReadAsStringAsync();
+                    Task.WaitAll(stringTask);
+                    Body = (T)(object)stringTask.Result;
+                }
+                else if (typeof(Stream).IsAssignableFrom(typeof(T)))
+                {
+                    Body = (T)(object)Raw;
+                }
+                else
+                {
+                    var stringTask = response.Content.ReadAsStringAsync();
+                    Task.WaitAll(stringTask);
+                    Body = JsonConvert.DeserializeObject<T>(stringTask.Result);
+                }
+            }
+
+            foreach (var header in response.Headers)
+            {
+                Headers.Add(header.Key, header.Value.First());
+            }
+        }
+    }
+}
